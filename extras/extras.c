050428865c4eae4cda1a545c264eb7183ca760c0
mateusmoutinho <mateusmoutinho01@gmail.com>
2023-08-24 11:58:34 -0300

    at

--- extras/extras.c
+++ extras/extras.c
@@ -4,5 +4,5 @@ void add_json_to_resource_and_delete_json(DtwResource *resource, cJSON *generate
     char *generated_json  = cJSON_Print(generated);
     dtw.resource.set_string(resource,generated_json);
     free(generated_json);
-    cJSON_Delete(element);
+    cJSON_Delete(generated);
 }
