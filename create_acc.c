f937d917b35e5a55b67c8c6521dcb93fde4f03bc
Wacoder Forever <sethomondi088@gmail.com>
2023-08-24 16:53:32 +0300

    re

---
+++ create_acc.c
@@ -0,0 +1,12 @@
+#include "dependencies/DoTheWord.h"
+#include "dependencies/clinput.h"
+
+typedef struct Person{
+    char *fname;
+    char *lname;
+    char *password;
+    float balance;
+
+}Person;
+
+Person create_acc(char *fname, )
