7c0cb9ce9b878e94b78bd68bcedf2d121b2cc26f
Wacoder Forever <sethomondi088@gmail.com>
2023-08-28 18:48:18 +0300

    acc

---
+++ database/account/account.h
@@ -0,0 +1,11 @@
+DtwResource *get_account_resource(struct DtwResource *database);
+
+cJSON *get_account_json(struct DtwResource *database);
+
+char *find_account_id_by_name(struct DtwResource *database,const char *name);
+
+void rename_account(struct DtwResource *database,const char *id,const char *new_name);
+
+void create_account(struct DtwResource *database,const char *name);
+
+void remove_account(struct DtwResource *database,const char *name);
