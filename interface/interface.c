4e2bb44d11693e6ac81b58a151246368bc9b1972
Wacoder Forever <sethomondi088@gmail.com>
2023-08-30 12:09:44 +0300

    TY

--- interface/interface.c
+++ interface/interface.c
@@ -1,5 +1,5 @@
 char *get_interface_name(CliEntry *entry){
-    CliFlag *name_flag = cli.entry.get_flag(entry,"name | n ",CLI_CASE_SENSITIVE);
+    CliFlag *name_flag = cli.entry.get_flag(entry,"name | n ",CLI_NOT_CASE_SENSITIVE);
 
     if(name_flag->exist == false){
         printf("name its required\n");
@@ -9,12 +9,12 @@ char *get_interface_name(CliEntry *entry){
         printf("name flag its empty\n");
         return NULL;
     }
-    return cli.flag.get_str(name_flag,0,CLI_CASE_SENSITIVE);
+    return cli.flag.get_str(name_flag,0,CLI_NOT_CASE_SENSITIVE);
 
 }
 
 char *get_new_name(CliEntry *entry){
-    CliFlag *new_name_flag = cli.entry.get_flag(entry,"new_name | n ",CLI_CASE_SENSITIVE);
+    CliFlag *new_name_flag = cli.entry.get_flag(entry,"new_name | n ",CLI_NOT_CASE_SENSITIVE);
 
     if(new_name_flag->exist == false){
         printf("new name is required\n");
@@ -24,6 +24,6 @@ char *get_new_name(CliEntry *entry){
         printf("new name flag is empty\n");
         return NULL;
     }
-    return cli.flag.get_str(new_name_flag,0,CLI_CASE_SENSITIVE);
+    return cli.flag.get_str(new_name_flag,0,CLI_NOT_CASE_SENSITIVE);
 
 }
